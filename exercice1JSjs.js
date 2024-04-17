2cf0f4a00cff197c51cd716978b85c55b9709ec6
MarmeyAFA <mariusmeyran2@gmail.com>
2024-04-17 08:52:56 +0200

    fonction 1

--- exercice1JSjs.js
+++ exercice1JSjs.js
@@ -1,6 +1,11 @@
-function C_to_Far(temp) {
-    let c = temp ;
-    let Fahrenheit = c * 1.8 +35 ;
-    return Fahrenheit ;
+function C_to_Far(c) {
+    return  c * 1.8 +32;
+}
+
+function afficher() {
+    var data = document.getElementById("celsiusInput").value ;
+    c = Number(data);
+    document.getElementById("resultat").innerHTML = 
+        "Température en Fahrenheit : " + C_to_Far(c);
 }
 
